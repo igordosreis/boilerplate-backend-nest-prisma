1b53cc4aee856b67091613c3a0c47d7ca1c2d6a6
Igor dos Reis <igorreis@gmail.com>
2024-04-16 15:38:43 -0300

    :boom: fix: commitlint whitelisted types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,39 +1,3 @@
-// module.exports = {
-//   extends: ['@commitlint/config-conventional'],
-//   rules: {
-//     'type-enum': [
-// 			2,
-// 			'always',
-// 			[
-// 				'build',
-//         ':package: build',
-// 				'chore',
-//         ':truck: chore',
-// 				'ci',
-//         ':bricks: ci',
-// 				'docs',
-// 				':books: docs',
-// 				'feat',
-//         ':sparkles: feat',
-// 				'fix',
-//         ':boom: fix',
-// 				'perf',
-// 				':zap: perf',
-// 				'refactor',
-// 				':recycle: refactor',
-// 				'revert',
-// 				':boom: revert',
-// 				'style',
-// 				':ok_hand: style',
-// 				'test',
-//         ':test_tube: test',
-// 			],
-// 		],
-//   },
-// };
-
-// commitlint.config.js
-// emojis like "✅ ", "😂 ", ...
 const matchAnyEmojiWithSpaceAfter =
   /(?<!:)(?::package: build|:truck: chore|:bricks: ci|:books: docs|:sparkles: feat|:boom: fix|:zap: perf|:recycle: refactor|:boom: revert|:ok_hand: style|:test_tube: test|build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)\b(?!_)/;
 const matchOptionalTicketNumberWithSpaceAfter = /(?:\((T-\d+)\)\s)?/; // "[T-4605] ", "[T-1]"
@@ -97,51 +61,10 @@ ${(allowedTypes)
           }
           return [true, ""];
         },
-        // "explained-emoji-enum": (parsed, _when, allowedTypes) => {
-        //   const { type } = parsed;
-        //   if (type && (allowedTypes).includes(type)) {
-        //     return [
-        //       false,
-        //       `emoji must be one of:
-        //       ${(allowedTypes)
-        //         .map((type) => `${type}`)
-        //         .join("\n")}`,
-        //     ];
-        //   }
-        //   return [true, ""];
-        // },
       },
     },
   ],
   rules: {
     "header-match-team-pattern": [2, "always"],
-    // "explained-emoji-enum": [
-    //   2,
-    //   "always",
-			// [
-			// 	'build',
-      //   ':package: build',
-			// 	'chore',
-      //   ':truck: chore',
-			// 	'ci',
-      //   ':bricks: ci',
-			// 	'docs',
-			// 	':books: docs',
-			// 	'feat',
-      //   ':sparkles: feat',
-			// 	'fix',
-      //   ':boom: fix',
-			// 	'perf',
-			// 	':zap: perf',
-			// 	'refactor',
-			// 	':recycle: refactor',
-			// 	'revert',
-			// 	':boom: revert',
-			// 	'style',
-			// 	':ok_hand: style',
-			// 	'test',
-      //   ':test_tube: test',
-			// ],
-    // ],
   },
 };
