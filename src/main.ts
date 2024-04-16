f0bf60b25d257deda45e2a04ff88d4b0c68ba8ef
Igor dos Reis <igorreis@gmail.com>
2024-04-16 17:26:47 -0300

    feat: add error global filter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpErrorFilter } from '@shared/filters/http-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useGlobalFilters(new HttpErrorFilter());
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/shared/filters/http-error.filter.ts
@@ -0,0 +1,22 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpErrorFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp(),
+      response = ctx.getResponse(),
+      request = ctx.getRequest(),
+      status = exception.getStatus();
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
